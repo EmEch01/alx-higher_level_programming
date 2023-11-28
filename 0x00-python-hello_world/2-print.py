ecd1354c2a1ad9cdb6b389d6e354f192265abbd1
EmEch01 <rhamzee13@gmail.com>
2023-11-28 14:30:30 -0800

    Python, Hello World

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("Programming is like building a multilingual puzzle")
+string = '\"Programming is like building a multilingual puzzle\n'
+exact_string = string[:-1]
+print(exact_string)
