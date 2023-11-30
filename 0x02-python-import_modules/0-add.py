088e1cb3f3fb533ae09111694017866f5174e966
EmEch01 <rhamzee13@gmail.com>
2023-11-30 13:47:12 -0800

    Import module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 
-
 # Import add function from add_0.py
 from add_0 import add
 
-# run directly without executing when called via import
 if __name__ == "__main__":
 
-    # Assign a value to a variable called a
     a = 1
-
-    # Assign a value to a variable called b
     b = 2
 
-# Print the result of the addition with the string formatting method
-print("{} + {} = {}".format(a, b, add(a, b)))
+    # Print the result of the addition with the string formatting method
+    print("{} + {} = {}".format(a, b, add(a, b)))
