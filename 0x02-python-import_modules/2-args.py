f51ff684a24fda249b93e8298047d7e22f98240b
EmEch01 <rhamzee13@gmail.com>
2023-11-30 11:32:53 -0800

    Import module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-
 from sys import argv
 
+
 def main():
+# Get the number of args
     args = len(argv[1:])
-    if (args == 0):
+
+# Print the number and list of args
+    if args == 0:
         print("0 arguments.")
     else:
         print("{} argument{}:".format(args, 's' if args > 1 else ''))
     for i, arg in enumerate(argv[1:], start=1):
         print("{}: {}".format(i, arg))
 
-# prevent code from being executed when called via import
+# code should not be executed when called via import
 if __name__ == "__main__":
     main()
