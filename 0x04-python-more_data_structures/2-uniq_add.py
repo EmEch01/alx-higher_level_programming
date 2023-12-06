81565e2b0614532bc8f555d5adef6a79fcb41dcd
EmEch01 <rhamzee13@gmail.com>
2023-12-06 12:27:38 -0800

    More Data Structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    unique_sum = set(my_list)
-    num = 0
+    """
+    This function adds all unique integers in a list.
 
-    for i in uniq_list:
-        num += i
+    Args:
+        my_list: The list of integers to process. Defaults to an empty list.
 
-    return (num)
+    Returns:
+        The sum of all unique integers in the list.
+    """
+
+    sum_of_uniques = 0
+    seen = set()  # Use a set to track seen integers
+
+    # Loop through each element in the list
+    for num in my_list:
+        # Check if the element is an integer
+        if isinstance(num, int):
+            # Check if the element is not already seen
+            if num not in seen:
+                # Add the element to the set of seen integers
+                seen.add(num)
+                # Add the element to the sum
+                sum_of_uniques += num
+
+    return sum_of_uniques
