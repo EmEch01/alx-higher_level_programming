116fc72813e508e1a7a60e67beb5bb292ef09a71
EmEch01 <rhamzee13@gmail.com>
2023-12-06 17:56:03 -0800

    More Data Structures

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    """
-    Squares all elements of a matrix using map.
-
-    Args:
-        matrix: A two-dimensional list of integers.
-
-    Returns:
-        A new matrix with squared elements.
-    """
-    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
+    return list(map(lambda x: list(map(lambda e: e**2, x)), matrix))
